467d5f2a184be63abecc2e4de8ece9c5c5f3fa91
rohith-commits <rohithssiddeshwara@gmail.com>
2024-06-06 08:27:30 +0530

    basics

---
+++ index.js
@@ -0,0 +1,70 @@
+// let name = "Rohith",
+//     age = 21,
+//     networth = -1.62
+
+// console.log(name,age,networth)
+
+// console.log(NaN*2)
+
+// let bigNumber = 10000n**10000n
+// console.log(bigNumber)
+
+// let name = "Rohith"
+// console.log(`name is ${name}`)
+
+// let obj = {}
+// console.log(typeof(obj))
+
+// alert("This is just to give alert")
+// let message = prompt("Here you can type the message it is returned","this is default value")
+// let boolean = confirm("This will return true if pressed OK and false if presed CANCELED")
+
+// let num = "12 "
+// console.log(Number(num))
+
+
+// console.log(2+3+"4")
+
+// console.log(+"2"+ +"2")
+
+// console.log(34-(a=2+3))
+
+// let a,b,c;
+
+// a = b = c = 2+3
+
+// console.log(a,b,c)
+
+
+// i = 2
+// i +=1
+// console.log(i)
+
+// let x = (2+2,3+3)
+// console.log(x)
+
+// for(let i; i = (2+2,3+3); i++){
+//     console.log(i)
+// }
+
+// console.log("A">"a")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
